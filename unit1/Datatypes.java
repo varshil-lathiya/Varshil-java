2b53811f9f93735572536fe1339393a93c25e9ee
varshil-lathiya <149300316+varshil-lathiya@users.noreply.github.com>
2023-12-26 23:38:29 +0530

    Add files via upload
    Datatypes in java

---
+++ unit1/Datatypes.java
@@ -0,0 +1,26 @@
+package unit1;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+        int v = 10;
+        System.out.println(v);
+        float f = 59.4f;
+        System.out.println(f);
+        char ch = 'V';
+        System.out.println(ch);
+        boolean d = true;
+        System.out.println(d);
+        byte range = 124;
+        System.out.println(range);
+        short s = -1998;
+        System.out.println(s);
+        long l = -16478263L;
+        System.out.println(l);
+        double x = -24.12;
+        System.out.println(x);;
+        
+	}
+
+}
