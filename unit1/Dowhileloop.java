d15c43cb4b1a821c45649907b2f8b82b3fe5bf4e
varshil-lathiya <149300316+varshil-lathiya@users.noreply.github.com>
2023-12-27 01:25:09 +0530

    Add files via upload

---
+++ unit1/Dowhileloop.java
@@ -0,0 +1,15 @@
+package unit1;
+
+public class Dowhileloop {
+
+	public static void main(String[] args) {
+		 int sum = 0;
+	        int i = 1;
+
+	        do {
+	            sum += i;
+	            System.out.println("when i = " + i + " the sum of variable is = " + sum);
+	            i++;
+	        } while (i <= 10);
+	}
+}
