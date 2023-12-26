9219f968dac23d9c89011700539f7aa3e2b7f323
varshil-lathiya <149300316+varshil-lathiya@users.noreply.github.com>
2023-12-27 00:15:15 +0530

    Add files via upload

---
+++ unit1/Whileloop.java
@@ -0,0 +1,16 @@
+package unit1;
+
+public class Whileloop {
+
+	public static void main(String[] args){
+		 int i = 1 ;
+		 int sum=0;
+		 
+	     while (i <= 10){
+	    	 sum=sum+i;
+	    	 System.out.println("when i = " + i + " the sum of variable is = " + sum);
+	     	 i++;
+	     }
+
+	}
+}
